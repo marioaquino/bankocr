78ea47639bc40795d84dd2749e8fe719bbc68b97
Mario Aquino <mario.e.aquino@gmail.com>
2013-11-28 20:58:11 -0600

    Added file reader that grabs account lines and passes them on.

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -1,5 +1,11 @@
 module OCR
-  def self.read_file(path, &account_handler)
-    account_handler.call(nil)
+  LINES_PER_ACCOUNT = 4
+  ACCOUNT_LINES_SIZE = 3
+
+  def self.read_file(path, &account_parser)
+    IO.foreach(path).each_slice(LINES_PER_ACCOUNT).take_while do |lines|
+      account_parser.call(lines.take(ACCOUNT_LINES_SIZE))
+    end
+    account_parser.call(nil)
   end
 end
--- spec/lib/ocr_spec.rb
+++ spec/lib/ocr_spec.rb
@@ -2,25 +2,40 @@ require_relative '../../lib/ocr'
 require 'tempfile'
 
 describe OCR do
-  context 'reading from an empty file' do
-    subject {
-      Tempfile.open('empty.txt') do |f|
-        OCR.read_file(f.path, &account_handler)
-      end
-    }
+  subject {
+    Tempfile.open('file.txt') do |f|
+      populate_file.(f)
+      f.close
+      OCR.read_file(f.path, &account_parser)
+    end
+  }
 
-    context 'calls the account number handler' do
-      let(:account_handler) { -> (acct_number_or_nil) {raise 'Must be called'} }
+  context 'reading from an empty file' do
+    let(:populate_file) { ->(file) {} }
+
+    context 'calls the account number parser' do
+      let(:account_parser) { -> (acct_number_or_nil) {raise 'Must be called'} }
       it 'blows up' do
         expect{subject}.to raise_error
       end
     end
 
     context 'yields a nil when the end of the file has been reached' do
-      let(:account_handler) { -> (acct_number_or_nil) { acct_number_or_nil.should be_nil} }
+      let(:account_parser) { -> (acct_number_or_nil) { acct_number_or_nil.should be_nil} }
       it { subject }
     end
   end
 
-  context 'reading from a non-empty file'
+  context 'reading from a non-empty file' do
+    let(:line) { "\n" }
+    let(:call_accumulator) { [] }
+    let(:account_parser) { -> (lines) { call_accumulator << lines } }
+    let(:populate_file) { ->(file) { file.write line * 8 } }
+
+    before { subject }
+
+    it 'passes lines in groups of 3 to the account number parser, leaving off the blank line' do
+      call_accumulator.should == [[line, line, line], [line, line, line], nil]
+    end
+  end
 end
